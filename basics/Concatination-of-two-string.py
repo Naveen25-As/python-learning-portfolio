6c38905bc67b9d4bfa0e0a41a2db428de521f056
Naveen asundi <naveenasundi03@gmail.com>
2026-07-02 16:48:32 +0530

    Concatination of two strings

---
+++ basics/Concatination-of-two-string.py
@@ -0,0 +1,6 @@
+# Concantination of a two strings.
+str1  = "Naveen"
+str2 = "Asundi"
+print("String 1:",str1) # string1
+print("String 2:",str2) # string2
+print("Concatination of two strings:",str1+str2) #Concatination
