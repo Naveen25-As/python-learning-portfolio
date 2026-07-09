b2031566eadb71fd253308bfd2fc28a57f689fbf
Naveen asundi <naveenasundi03@gmail.com>
2026-07-09 17:29:15 +0530

    Check if Two Strings are Anagrams

---
+++ basics/Strings/Check-if-Two-Strings-are-Anagrams.py
@@ -0,0 +1,8 @@
+# Check if Two Strings are Anagrams
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
+if sorted(str1.lower()) == sorted(str2.lower()):
+    print("Anagrams")
+else:
+    print("Not Anagrams")
