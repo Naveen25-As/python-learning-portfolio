5bbd450abd338784c8850a1e71332efe903cb899
Naveen25-As <naveenasundi03@gmail.com>
2026-08-02 11:32:42 +0530

    2.Access List Elements.

---
+++ basics/lists/2.Access-List-Elements.py
@@ -0,0 +1,7 @@
+# 2.Access List Elements.
+
+fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+
+print("First:",fruits[0])  # Accessing the first element
+print("Second:",fruits[1])  # Accessing the second element
+print("Third:",fruits[2])  # Accessing the third element 
